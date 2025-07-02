f07d624364aecb40ea91765d2ea86d5f3031ecc7
Steven01234567 <steven.bin.chen17@gmail.com>
2025-07-02 18:39:57 -0400

    Added Models and ViewModels Folders. Added Entity class

---
+++ Models/Entity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Entity
+    {
+        /// private variables
+        private int _xCoordinate;
+        private int _yCoordinate;
+        private int _height;
+        private int _width;
+
+        /// public variables
+        public int XCoordinate
+        {
+            get => _xCoordinate;
+            set
+            {
+                _xCoordinate = value;
+            }
+        }
+        public int YCoordinate
+        {
+            get => _yCoordinate;
+            set
+            {
+                _yCoordinate = value;
+            }
+        }
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                _height = value;
+            }
+        }
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public Entity(int xCoordinate = 0, int yCoordinate = 0, int height = 1, int width = 1)
+        {
+            _xCoordinate = xCoordinate;
+            _yCoordinate = yCoordinate;
+            _height = height;
+            _width = width;
+        }
+    }
+}
